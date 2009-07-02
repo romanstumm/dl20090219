3ea0fdde384004e97c99c7f881668de38ed7532f
roman.stumm <roman.stumm@931aaaa6-feb9-11dd-88c7-29a3b14d5316>
2009-07-02 19:43:01 +0000

    version 2.4.4 DbfImporter behoben (externeId) Drucken korrigiert

--- liga/src/de/liga/dart/Application.java
+++ liga/src/de/liga/dart/Application.java
@@ -2,5 +2,5 @@ package de.liga.dart;
 
 public interface Application {
     String APPLICATION_NAME = "Ligaverwaltung";
-    String APPLICATION_VERSION = "2.4.3";
+    String APPLICATION_VERSION = "2.4.4";
 }
