af3c28db58c93ba1f8f0262e7900e7fd6e333824
Stephan.Pudras <Stephan.Pudras@931aaaa6-feb9-11dd-88c7-29a3b14d5316>
2009-09-02 18:14:17 +0000

    Rejected commit: Default

---
+++ trunk/gui/src/org/en/tealEye/printing/gui/EnvelopePrintFrameMethods.java
@@ -0,0 +1,25 @@
+package org.en.tealEye.printing.gui;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Stephan Pudras
+ * Date: 02.09.2009
+ * Time: 10:42:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class EnvelopePrintFrameMethods {
+
+    public EnvelopePrintFrameMethods(){
+
+    }
+
+    public void epPrintBt(){
+        System.out.println("PrintBT");                 
+    }
+
+    public void epDeclineBt(){
+        System.out.println("DeclineBT");
+    }
+
+
+}
