d23edd46fb11aecc0dca04b46eafaec7f5e00a2d
Stephan.Pudras <Stephan.Pudras@931aaaa6-feb9-11dd-88c7-29a3b14d5316>
2009-09-17 20:56:30 +0000

    Rejected commit: Default

--- gui/src/org/en/tealEye/printing/gui/SenderAddressFrameMethods.java
+++ gui/src/org/en/tealEye/printing/gui/SenderAddressFrameMethods.java
@@ -1,9 +1,13 @@
 package org.en.tealEye.printing.gui;
 
+import org.en.tealEye.guiServices.GlobalGuiServiceImpl;
+import org.en.tealEye.guiServices.GlobalGuiService;
+
 import java.awt.*;
 import javax.swing.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,44 +21,46 @@ public class SenderAddressFrameMethods {
     private Map<String, Component> components = new HashMap<String, Component>();
     private Component targetComponent;
     private JFrame form;
+    private GlobalGuiService service;
 
     public SenderAddressFrameMethods(Map<String, Component> components, Object form, Component targetComponent) {
         this.components = components;
         this.targetComponent = targetComponent;
         this.form = (JFrame) form;
+        service = new GlobalGuiService();
     }
 
     public void senderFillTextCustom(){
-        String str = ((JButton)targetComponent).getToolTipText();
-        String[] tA = str.split(" | ");
-        String[] t = new String[5];
-        int i = 0;
-        for(String s: tA){
-            if(!s.contains("|")){
-            t[i] = s.trim();
-            i++;
-            }
-        }
-
-        for(String s: t){
-            ((JTextField)components.get("senderAddressFrameCorpTF")).setText(t[0]);
-            ((JTextField)components.get("senderAddressFrameNameTF")).setText(t[1]);
-            ((JTextField)components.get("senderAddressFrameStreetTF")).setText(t[2]);
-            ((JTextField)components.get("senderAddressFramePLZTF")).setText(t[3]);
-            ((JTextField)components.get("senderAddressFrameLocationTF")).setText(t[4]);
-        }
+            ((JTextField)components.get("senderAddressFrameCorpTF")).setText(service.getProperty("senderCorp"));
+            ((JTextField)components.get("senderAddressFrameNameTF")).setText(service.getProperty("senderName"));
+            ((JTextField)components.get("senderAddressFrameStreetTF")).setText(service.getProperty("senderStreet"));
+            ((JTextField)components.get("senderAddressFramePLZTF")).setText(service.getProperty("senderPLZ"));
+            ((JTextField)components.get("senderAddressFrameLocationTF")).setText(service.getProperty("senderLocation"));
     }
 
     public void senderAddressFrameAcceptBt(){
-        ((JButton)targetComponent).setToolTipText(((JTextField)components.get("senderAddressFrameCorpTF")).getText()+" | "+
+            ((JButton)targetComponent).setToolTipText(((JTextField)components.get("senderAddressFrameCorpTF")).getText()+" | "+
                                                   ((JTextField)components.get("senderAddressFrameNameTF")).getText()+" | "+
                                                   ((JTextField)components.get("senderAddressFrameStreetTF")).getText()+" | "+
                                                   ((JTextField)components.get("senderAddressFramePLZTF")).getText()+" | "+
                                                   ((JTextField)components.get("senderAddressFrameLocationTF")).getText());
-        form.dispose();
+            Properties props = new Properties();
+
+            props.setProperty("senderCorp", ((JTextField)components.get("senderAddressFrameCorpTF")).getText());
+            props.setProperty("senderName",((JTextField)components.get("senderAddressFrameNameTF")).getText());
+            props.setProperty("senderStreet",String.valueOf(((JTextField)components.get("senderAddressFrameStreetTF")).getText()));
+            props.setProperty("senderPLZ", String.valueOf(((JTextField)components.get("senderAddressFramePLZTF")).getText()));
+            props.setProperty("senderLocation", ((JTextField)components.get("senderAddressFrameLocationTF")).getText());
+
+            service.updateProps(props);
+        //form.dispose();
     }
     
     public void senderAddressFrameDeclineBt(){
         form.dispose();
+    }
+
+    public void DisposeMethod(){
+
     }
 }
--- gui/src/org/en/tealEye/printing/service/EnvelopeBook.java
+++ gui/src/org/en/tealEye/printing/service/EnvelopeBook.java
@@ -1,8 +1,13 @@
 package org.en.tealEye.printing.service;
 
+import javax.imageio.ImageIO;
 import java.awt.print.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.AffineTransform;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,32 +17,115 @@ import java.awt.geom.Rectangle2D;
  * To change this template use File | Settings | File Templates.
  */
 public class EnvelopeBook implements Printable {
-    private String[] g;
+    private String[] grp;
 
-    public EnvelopeBook(String[] g) {
-        this.g = g;
+    private int xAxisAddress = 0;
+    private int yAxisAddress = 0;
+
+    private int xAxisSender = 0;
+    private int yAxisSender = 0;
+
+    private int xAxisGraphic = 0;
+    private int yAxisGraphic = 0;
+
+    private Font senderFont;
+    private Font addressFont;
+    private String[] sender;
+    private String graphicPath;
+    private boolean senderInCorner;
+    private boolean showGraphic;
+    private boolean showSender;
+    private PageFormat pf;
+    private int maximumPages;
+
+    public EnvelopeBook(String[] g, PageFormat pf, int xAxisAddress, int yAxisAddress, int xAxisSender, int yAxisSender, int xAxisGraphic, int yAxisGraphic, String[] sender, String graphicPath, Font addressFont, Font senderFont, boolean showGraphic, boolean leftCornerSender, boolean showSender, int maximumPages) {
+        this.grp = g;
+        this.pf = pf;
+        this.xAxisAddress = xAxisAddress;
+        this.yAxisAddress = yAxisAddress;
+        this.xAxisSender = xAxisSender;
+        this.yAxisSender = yAxisSender;
+        this.xAxisGraphic = xAxisGraphic;
+        this.yAxisGraphic = yAxisGraphic;
+        this.senderFont  = senderFont;
+        this.addressFont = addressFont;
+        this.sender = sender;
+        this.graphicPath = graphicPath;
+        this.senderInCorner = leftCornerSender;
+        this.showGraphic = showGraphic;
+        this.showSender = showSender;
+        this.maximumPages = maximumPages;
     }
   @Override
   public int print(Graphics g, PageFormat pf, int pageIndex) throws PrinterException {
     System.out.println("Page index = " + pageIndex);
     // pageIndex 1 corresponds to page number 2.
-    if (pageIndex > 150)
+    if (pageIndex > maximumPages)
       return Printable.NO_SUCH_PAGE;
 
     Graphics2D g2 = (Graphics2D) g;
 
-    double w = pf.getImageableWidth();
-    double h = pf.getImageableHeight();
 
-    int xo = (int) pf.getImageableX();
-    int yo = (int) pf.getImageableY();
+        FontMetrics sfm = g2.getFontMetrics(senderFont);
+        FontMetrics afm = g2.getFontMetrics(addressFont);
+        int a = sfm.stringWidth(sender[0]);
+        int b = sfm.stringWidth(sender[1]);
+        int c = sfm.stringWidth(sender[2]);
+        int d = sfm.stringWidth(sender[3]);
+        int e = sfm.stringWidth(sender[4]);
+        g2.setColor(Color.black);
+        if(showGraphic){
+            BufferedImage img = null;
+            File f = new File(graphicPath);
+            if(f.canRead()){
+            try {
+                img = ImageIO.read(f);
+            } catch (IOException x) {
+                x.printStackTrace();
+            }
+            }
+            AffineTransform bio = new AffineTransform();
+            bio.scale(0.1,0.1);
+            g2.drawImage(img,(int)(pf.getImageableWidth()*0.6)+xAxisGraphic,(int)(pf.getImageableHeight()*0.5)+yAxisGraphic,(int)(pf.getImageableWidth()*0.4),afm.getHeight()*8,Color.white,null);
 
-    g2.setColor(Color.black);  
+        }
+        if(showSender){
+            g2.setFont(senderFont);
+                if(senderInCorner){
+                    int i = 0;
+                    for(String s : sender){
+                        g2.drawString(s,(int)(pf.getImageableWidth()*0.1)+xAxisSender, (int)(pf.getImageableHeight()*0.1)+sfm.getHeight()*i+sfm.getHeight()/2+yAxisSender);
+                        i++;
+                    }
+               }else{
+                    g2.drawString(sender[0],(int)(pf.getImageableWidth()*0.6)+xAxisSender, (int)(pf.getImageableHeight()*0.5)-afm.getHeight()/2+yAxisSender);
+                    g2.drawString(sender[1],(int)(pf.getImageableWidth()*0.6)+5+a+xAxisSender, (int)(pf.getImageableHeight()*0.5)-afm.getHeight()/2+yAxisSender);
+                    g2.drawString(sender[2],(int)(pf.getImageableWidth()*0.6)+10+a+b+xAxisSender, (int)(pf.getImageableHeight()*0.5)-afm.getHeight()/2+yAxisSender);
+                    g2.drawString(sender[3],(int)(pf.getImageableWidth()*0.6)+15+a+b+c+xAxisSender, (int)(pf.getImageableHeight()*0.5)-afm.getHeight()/2+yAxisSender);
+                    g2.drawString(sender[4],(int)(pf.getImageableWidth()*0.6)+20+a+b+c+d+xAxisSender, (int)(pf.getImageableHeight()*0.5)-afm.getHeight()/2+yAxisSender);
+                    g2.drawLine((int)(pf.getImageableWidth()*0.6)+xAxisSender,(int)((pf.getImageableHeight()*0.5)-afm.getHeight()/2)+1+yAxisSender,(int)(pf.getImageableWidth()*0.6)+20+a+b+c+d+e+xAxisSender,(int)((pf.getImageableHeight()*0.5)-afm.getHeight()/2)+1+yAxisSender);
+                }
+        }
+        g2.setFont(addressFont);
+        int i = 0;
+        for(String s: grp){
+            if(i==0){
+                if(s.equals("")){
+                    i--;
+                }else
+                g2.drawString(s,(int)(pf.getImageableWidth()*0.6)+xAxisAddress,(int)(pf.getImageableHeight()*0.5)+ yAxisAddress);
+            }
+            else{
 
-    g2.drawString(this.g[0],300,200);
-    g2.drawString(this.g[1],300,220);
-    g2.drawString(this.g[2],300,240);
-    g2.drawString(this.g[3],300,260);
+                if(s.equals("")){
+                    i--;
+                }
+                else{
+                    g2.drawString(s,(int)(pf.getImageableWidth()*0.6)+xAxisAddress,(int)(pf.getImageableHeight()*0.5)+afm.getHeight()*i+(afm.getHeight()/2)+ yAxisAddress);
+                }
+            }
+            i++;
+        }
 
     return Printable.PAGE_EXISTS;
   }
