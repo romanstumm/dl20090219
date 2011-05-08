8ad77b566ef7e1200d1cd738c8fa94d446b3b1b1
roman.stumm <roman.stumm@931aaaa6-feb9-11dd-88c7-29a3b14d5316>
2011-05-08 17:47:16 +0000

    ftp upload implementiert, noch ungetestet commons-net-2.2 für FTP Client hinzugefügt

--- liga/src/de/liga/dart/fileimport/FtpWebIO.java
+++ liga/src/de/liga/dart/fileimport/FtpWebIO.java
@@ -2,6 +2,11 @@ package de.liga.dart.fileimport;
 
 import de.liga.dart.exception.DartException;
 import de.liga.dart.model.Liga;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
 
 import java.io.*;
 import java.util.HashMap;
@@ -18,6 +23,8 @@ public class FtpWebIO {
     // target files
     protected static final Map<String, String> WEB_DIRS = new HashMap();
     protected static String rangSuffix = "_rang.html", planSuffix = "_plan.html";
+    private static String server, user, password, ftpDirPlan, ftpDirRang;
+    private static final Log log = LogFactory.getLog(FtpWebIO.class);
 
     public static void setRangSuffix(String rangSuffix) {
         FtpWebIO.rangSuffix = rangSuffix;
@@ -37,6 +44,20 @@ public class FtpWebIO {
 
     public static void clear() {
         WEB_DIRS.clear();
+        server = null;
+        user = null;
+        password = null;
+    }
+
+    public static void setFtpServerUserPassword(String s, String u, String p) {
+        server = s;
+        user = u;
+        password = p;
+    }
+
+    public static void setFtpPlanRangDirs(String p, String r) {
+        ftpDirPlan = p;
+        ftpDirRang = r;
     }
 
     public void copyRangHtmlToWebdir(Liga liga) throws DartException, IOException {
@@ -45,9 +66,13 @@ public class FtpWebIO {
         File sourcedir = new File(syncdir, "HTARCHIV");
 
         // webdir = target dir
-        String webdir = getWebdir(liga);
-        File targetdir = new File(webdir, "rangliste");
+        File targetdir = getWebdirRang(liga);
         copyFiles(sourcedir, targetdir, rangSuffix.toLowerCase());
+    }
+
+    private File getWebdirRang(Liga liga) {
+        String webdir = getWebdir(liga);
+        return new File(webdir, "rangliste");
     }
 
     private String getWebdir(Liga liga) {
@@ -62,9 +87,13 @@ public class FtpWebIO {
         File sourcedir = new File(syncdir, "HTARCHIV");
 
         // webdir = target dir
-        String webdir = getWebdir(liga);
-        File targetdir = new File(webdir, "spielplan");
+        File targetdir = getWebdirPlan(liga);
         copyFiles(sourcedir, targetdir, planSuffix.toLowerCase());
+    }
+
+    private File getWebdirPlan(Liga liga) {
+        String webdir = getWebdir(liga);
+        return new File(webdir, "spielplan");
     }
 
     private void copyFiles(File sourcedir, File targetdir, final String suffix) throws IOException {
@@ -117,12 +146,104 @@ public class FtpWebIO {
         }
     }
 
-    public void uploadRangWebdir(Liga liga) {
-        // todo nyi
-
+    public void uploadRangWebdir(Liga liga) throws IOException {
+        FTPClient ftp = prepareFTP();
+        try {
+            File webdir = getWebdirRang(liga);
+            if (ftpDirRang != null && ftpDirRang.length() > 0)
+                ftp.changeWorkingDirectory(ftpDirRang);
+            for (File each : webdir.listFiles()) {
+                if (each.isFile())
+                    ftpStoreFile(each.getPath(), each.getName(), ftp);
+            }
+        } finally {
+            ftpDisconnect(ftp);
+        }
     }
 
-    public void uploadPlanWebdir(Liga liga) {
-        // todo nyi
+    public void uploadPlanWebdir(Liga liga) throws IOException {
+        FTPClient ftp = prepareFTP();
+        try {
+            File webdir = getWebdirPlan(liga);
+            if (ftpDirPlan != null && ftpDirPlan.length() > 0)
+                ftp.changeWorkingDirectory(ftpDirPlan);
+            for (File each : webdir.listFiles()) {
+                if (each.isFile())
+                    ftpStoreFile(each.getPath(), each.getName(), ftp);
+            }
+        } finally {
+            ftpDisconnect(ftp);
+        }
+    }
+
+    private FTPClient prepareFTP() throws IOException {
+        FTPClient ftp;
+        ftp = new FTPClient();
+        ftpConnect(server, ftp);
+        ftpLogin(user, password, true, ftp);
+        return ftp;
+    }
+
+    private void ftpDisconnect(FTPClient ftp) throws IOException {
+        if (ftp.isConnected()) {
+            ftp.disconnect();
+        }
+    }
+
+    private void ftpStoreFile(String local, String remote, FTPClient ftp) throws IOException {
+        InputStream input;
+        log.info("ftp uploading file " + local + " to " + remote + "...");
+        input = new FileInputStream(local);
+        try {
+            boolean success = ftp.storeFile(remote, input);
+            if (success)
+                log.info("ftp upload of file " + remote + " done: successful");
+            else
+                log.error("ftp upload of file " + remote + " done: failed");
+        } finally {
+            input.close();
+        }
+    }
+
+    private void ftpLogin(String username, String password, boolean binaryTransfer, FTPClient ftp) {
+        try {
+            if (!ftp.login(username, password)) {
+                ftp.logout();
+            }
+
+            log.info("Remote system is " + ftp.getSystemName());
+
+            if (binaryTransfer)
+                ftp.setFileType(FTP.BINARY_FILE_TYPE);
+
+            // Use passive mode as default because most of us are
+            // behind firewalls these days.
+            ftp.enterLocalPassiveMode();
+        } catch (Exception e) {
+            throw new DartException("exception during FTP upload", e);
+        }
+    }
+
+    private void ftpConnect(String server, FTPClient ftp) {
+        try {
+            ftp.connect(server);
+            int reply = ftp.getReplyCode();
+
+            if (!FTPReply.isPositiveCompletion(reply)) {
+                ftp.disconnect();
+                throw new DartException("cannot connect to FTP");
+            }
+        } catch (IOException e) {
+            if (ftp.isConnected()) {
+                try {
+                    ftp.disconnect();
+                }
+                catch (IOException f) {
+                    // do nothing
+                }
+            }
+            throw new DartException("exception during FTP connect", e);
+        }
     }
 }
+
