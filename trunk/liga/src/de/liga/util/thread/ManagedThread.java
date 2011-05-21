c9b79805e95b2f36c509293c59b948e348b85dc7
roman.stumm <roman.stumm@931aaaa6-feb9-11dd-88c7-29a3b14d5316>
2011-05-21 08:53:23 +0000

    fake ftp fixed logging improved

--- trunk/liga/src/de/liga/util/thread/ManagedThread.java
+++ trunk/liga/src/de/liga/util/thread/ManagedThread.java
@@ -1,7 +1,6 @@
 package de.liga.util.thread;
 
 
-
 /**
  * This class provides a <code>Thread</code> with extensions for usage in a
  * managed environment.
@@ -9,73 +8,76 @@ package de.liga.util.thread;
  * @author Roman  Stumm
  */
 public class ManagedThread extends Thread {
-  public ManagedThread(Runnable target, String name) {
-    super(target, name);
-    manage();
-  }
+    public ManagedThread(Runnable target, String name) {
+        super(target, name);
+        manage();
+    }
 
-  /**
-   * Create instance and automatically add to management.
-   */
-  public ManagedThread() {
-    super();
-    manage();
-  }
+    /**
+     * Create instance and automatically add to management.
+     */
+    public ManagedThread() {
+        super();
+        manage();
+    }
 
-  /**
-   * Create instance and automatically add to management.
-   *
-   * @param name String with object name
-   */
-  public ManagedThread(String name) {
-    super(name);
-    manage();
-  }
+    /**
+     * Create instance and automatically add to management.
+     *
+     * @param name String with object name
+     */
+    public ManagedThread(String name) {
+        super(name);
+        manage();
+    }
 
-  /**
-   * Create instance and automatically add to management.
-   *
-   * @param run
-   */
-  public ManagedThread(Runnable run) {
-    super(run);
-    manage();
-  }
+    /**
+     * Create instance and automatically add to management.
+     *
+     * @param run
+     */
+    public ManagedThread(Runnable run) {
+        super(run);
+        manage();
+    }
 
-  public ManagedThread(ThreadGroup group, Runnable target) {
-    super(group, target);
-    manage();
-  }
+    public ManagedThread(ThreadGroup group, Runnable target) {
+        super(group, target);
+        manage();
+    }
 
-  public ManagedThread(ThreadGroup group, String name) {
-    super(group, name);
-    manage();
-  }
+    public ManagedThread(ThreadGroup group, String name) {
+        super(group, name);
+        manage();
+    }
 
-  public ManagedThread(ThreadGroup group, Runnable target, String name) {
-    super(group, target, name);
-    manage();
-  }
+    public ManagedThread(ThreadGroup group, Runnable target, String name) {
+        super(group, target, name);
+        manage();
+    }
 
-  public ManagedThread(ThreadGroup group, Runnable target, String name, long stackSize) {
-    super(group, target, name, stackSize);
-    manage();
-  }
+    public ManagedThread(ThreadGroup group, Runnable target, String name, long stackSize) {
+        super(group, target, name, stackSize);
+        manage();
+    }
 
-  protected final void manage() {
-    ThreadManager.getInstance().addThread(this);
-  }
+    protected final void manage() {
+        ThreadManager.getInstance().addThread(this);
+    }
 
-  /**
-   * call after run is complete when you subclass this class and overwrite the run-method
-   */
-  protected final void finished() {
-    // After run is completed remove thread from list
-    ThreadManager.getInstance().removeThread(this);
-  }
+    /**
+     * call after run is complete when you subclass this class and overwrite the run-method
+     */
+    protected final void finished() {
+        // After run is completed remove thread from list
+        ThreadManager.getInstance().removeThread(this);
+    }
 
-  public void run() {
-    super.run();
-    finished();
-  }
+    public void run() {
+        try {
+            super.run();
+        } finally {
+            finished();
+        }
+    }
 }
