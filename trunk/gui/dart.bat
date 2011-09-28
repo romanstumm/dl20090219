8529c2eb3f160bf0e4ead30ad69adb9113c2e112
roman.stumm <roman.stumm@931aaaa6-feb9-11dd-88c7-29a3b14d5316>
2011-09-28 12:06:52 +0000

    postgres upgrade 8.2 -> 8.4

--- trunk/gui/dart.bat
+++ trunk/gui/dart.bat
@@ -8,7 +8,7 @@ SET MYCP=%MYCP%;lib\commons-collections-3.2.1.jar;lib\commons-lang-2.4.jar;lib\c
 SET MYCP=%MYCP%;lib\dart-2.x.jar;lib\dartgui-2.x.jar;lib\dom4j-1.6.1.jar;lib\ehcache-1.2.3.jar
 SET MYCP=%MYCP%;lib\freemarker-2.3.15.jar;lib\gluegen-rt.jar;lib\hibernate-cglib-repack-2.1_3.jar
 SET MYCP=%MYCP%;lib\hibernate3.jar;lib\javassist-3.4.GA.jar;lib\jl1.0.jar;lib\jogl.jar;lib\jta-1.1.jar
-SET MYCP=%MYCP%;lib\liquidlnf.jar;lib\log4j-1.2.15.jar;lib\nimrodlf.j16.jar;lib\postgresql-8.2-506.jdbc4.jar
+SET MYCP=%MYCP%;lib\liquidlnf.jar;lib\log4j-1.2.15.jar;lib\nimrodlf.j16.jar;lib\postgresql-8.4-703.jdbc4.jar
 SET MYCP=%MYCP%;lib\slf4j-api-1.5.6.jar;lib\slf4j-log4j12-1.5.6.jar;lib\xpp3_min-1.1.4c.jar;lib\xstream-1.3.1.jar
 SET MYCP=%MYCP%;lib\substance.jar;lib\jl1.0.jar;lib\poi-3.2-FINAL-20081019.jar
 
